07bef1903e0d93c51a7d32c51f7f793306da42ac
CostaDog <tewillis@outlook.com>
2023-08-04 22:19:14 +0100

    hot warm cold colour

--- FindTheThimble/Program.cs
+++ FindTheThimble/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,16 +21,11 @@ namespace FindTheThimble
             int width = 20;
             int height = 10;
             Position thimble = new Position();
-            thimble.x = new Random().Next(0, width-1);
-            thimble.y = new Random().Next(0, height-1);
-            Console.WriteLine((thimble.x+1) + "," + (thimble.y+1));
-            Console.ReadKey();
-            //thimble.x = 0;
-            //thimble.y = 9;
+            thimble.x = new Random().Next(0, width - 1);
+            thimble.y = new Random().Next(0, height - 1);
+            
             Position guess = new Position();
-            
             List<Position> guesses = new List<Position>();
-            
             do
             {
                 Console.Clear();
@@ -37,27 +33,26 @@ namespace FindTheThimble
                 Console.WriteLine("");
                 for (int r = 0; r < height; r++)
                 {
-                    
                     for (int c = 0; c < width; c++)
                     {
-                        Position rc = new Position() { x=c, y=r};
-                        //if (guesses.ElementAt(r) == itemR.Value && guesses.ElementAt(c) == itemC.Key)
+                        Position rc = new Position() { x = c, y = r };
+
                         if (alreadyInput(rc, guesses))
                         {
+                            ConsoleColor colour = getColour(thimble, rc);
+                            Console.ForegroundColor = colour;
                             Console.Write("X");
+                            Console.ResetColor();
                         }
                         else
                         {
                             Console.Write("-");
                         }
-
                     }
                     Console.WriteLine();
                 }
-                guess.x = getUserInput($"Enter your X-Coordinate (between 1 and {width}");
-                guess.y = getUserInput($"Enter your Y-Coordinate (between 1 and {height}");
-                
-
+                guess.x = getUserInput($"Enter your X-Coordinate (between 1 and {width})");
+                guess.y = getUserInput($"Enter your Y-Coordinate (between 1 and {height})");
                 if (guess.x == thimble.x && guess.y == thimble.y)
                 {
                     playing = false;
@@ -66,23 +61,20 @@ namespace FindTheThimble
                 {
                     if (!alreadyInput(guess, guesses))
                     {
-                        if ((guess.x >= 0 && guess.x <=width) && (guess.y >= 0 && guess.y <= height)){
+                        if ((guess.x >= 0 && guess.x <= width) && (guess.y >= 0 && guess.y <= height))
+                        {
                             guesses.Add(guess);
-                            string distance = Distance(thimble, guess);
-                            Console.WriteLine($"You are {distance}, keep trying.");
+
+                            string temp = HWorC(thimble, guess);
+                            Console.WriteLine($"You are {temp}, keep trying.");
                         }
                         else
                         {
                             Console.WriteLine("Enter a valid coordinate");
-                           
                         }
                         Console.ReadKey();
-                        
                     }
-                    
-
                 }
-                
             } while (playing);
             Console.WriteLine("You found the thimble");
             Console.ReadKey();
@@ -96,14 +88,13 @@ namespace FindTheThimble
                 return userinput - 1;
             }
             return 0;
-
         }
         private static bool alreadyInput(Position guess, List<Position> guesses)
         {
-            return guesses.FindIndex( g => g.x==guess.x && g.y==guess.y )  >= 0;
+            return guesses.FindIndex(g => g.x == guess.x && g.y == guess.y) >= 0;
         }
 
-        private static string Distance(Position thimble, Position guess)
+        private static double Distance(Position thimble, Position guess)
         {
             int distanceY;
             int distanceX;
@@ -124,15 +115,34 @@ namespace FindTheThimble
                 distanceX = guess.x - thimble.x;
             }
             double distance = Math.Sqrt((distanceY * distanceY) + (distanceX * distanceX));
-            if (distance <= 3)
+            return distance;
+        }
+        private static ConsoleColor getColour(Position thimble, Position guess)
+        {
+            double d = Distance(thimble, guess);
+            if (d <= 3)
+            {
+                return ConsoleColor.Red;
+            }
+            if (d > 3 && d <= 5)
+            {
+                return ConsoleColor.Yellow;
+            }
+            return ConsoleColor.Cyan;
+        }
+        private static string HWorC(Position thimble, Position guess)
+        {
+            double d = Distance(thimble, guess);
+            if (d <= 3)
             {
                 return "hot";
             }
-            if (distance > 3 && distance <= 5)
+            if (d > 3 && d <= 5)
             {
                 return "warm";
             }
-                return "cold";
+            return "cold";
         }
+
     }
 }
